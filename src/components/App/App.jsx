dcc90036629f2dfa445f06ff109cd6f3d154f14f
Danny Mussie <dannymussie7@gmail.com>
2021-09-16 20:24:06 -0500

    created GET on App.jsx and added useState to file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+
 
 function App() {
+  let [galleryList, setGalleryList] = useState([]);
+  //GET
+  const getLike = () => {
+    axios({
+      method: 'GET',
+      url: '/like',
+    }).then((response) => {
+      console.log(response.data);
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log(error);
+      alert('Error making GET request');
+    })
+  }
     return (
       <div className="App">
         <header className="App-header">
