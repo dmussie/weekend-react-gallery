b73f5b3b875704f074983ea24efc6ba990fa75d6
Danny Mussie <dannymussie7@gmail.com>
2021-09-17 19:34:01 -0500

    deleted database.sql for now, created PUT in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import galleryItems from '../../../server/modules/gallery.data.js';
 import {useState, useEffect} from 'react';
+import { response } from 'express';
 
 
 
 function App() {
-  let [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    getGallery()
+  }, [])
+  
   //GET
-  const getLike = () => {
+  const getGallery = () => {
     axios({
       method: 'GET',
-      url: '/like',
+      url: '/gallery',
     }).then((response) => {
       console.log(response.data);
       setGalleryList(response.data);
@@ -20,6 +26,24 @@ function App() {
       alert('Error making GET request');
     })
   }
+
+  const [liked, setLiked] = useState(false);
+
+  //PUT
+  const handleLikes = () => {
+    console.log('Updating gallery to liked!');
+    const itemId = response.data.id;
+    axios({
+      method: 'PUT',
+      url: `/gallery/like/${itemId}`
+    }).then((response) => {
+      setLiked();
+    }).catch((error) => {
+      alert('Error with PUT.');
+      console.log('error with updating likes!', error);
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
