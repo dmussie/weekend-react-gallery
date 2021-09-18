ea7e2c7bc65a278f1da5926240dff4930d0889d3
Danny Mussie <dannymussie7@gmail.com>
2021-09-17 20:36:01 -0500

    edited getGallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import galleryItems from '../../../server/modules/gallery.data.js';
 import {useState, useEffect} from 'react';
-import { response } from 'express';
+//import { response } from 'express';
 
-
+ 
 
 function App() {
 
+  const [galleryList, setGalleryList] = useState([]);
+  console.log(galleryList);
   useEffect(() => {
     getGallery()
   }, [])
-  
+
   //GET
   const getGallery = () => {
     axios({
@@ -27,22 +28,22 @@ function App() {
     })
   }
 
-  const [liked, setLiked] = useState(false);
+  // const [liked, setLiked] = useState(false);
 
   //PUT
-  const handleLikes = () => {
-    console.log('Updating gallery to liked!');
-    const itemId = response.data.id;
-    axios({
-      method: 'PUT',
-      url: `/gallery/like/${itemId}`
-    }).then((response) => {
-      setLiked();
-    }).catch((error) => {
-      alert('Error with PUT.');
-      console.log('error with updating likes!', error);
-    })
-  }
+  // const handleLikes = () => {
+  //   console.log('Updating gallery to liked!');
+  //   const itemId = response.data.id;
+  //   axios({
+  //     method: 'PUT',
+  //     url: `/gallery/like/${itemId}`
+  //   }).then((response) => {
+  //     setLiked();
+  //   }).catch((error) => {
+  //     alert('Error with PUT.');
+  //     console.log('error with updating likes!', error);
+  //   })
+  // }
 
     return (
       <div className="App">
@@ -50,7 +51,12 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {galleryList.map(picture => (
+          <img key={picture.id} src="images/goat_small.jpg"/>
+        ))}
+          
+        
+        
       </div>
     );
 }
